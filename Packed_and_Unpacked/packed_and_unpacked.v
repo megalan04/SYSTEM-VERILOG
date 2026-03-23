3d6565c66aa96cb4a9f4ae89b039e90fa246950b
Megalan <megalan4444@gmail.com>
2026-03-23 15:57:26 +0530

    Create packed_and_unpacked.v

---
+++ Packed_and_Unpacked/packed_and_unpacked.v
@@ -0,0 +1,11 @@
+module unpacked_1d;
+  logic[31:0] array[7:0];
+  initial begin
+  array[4]='hdd_dd_dd_dd;
+  array[7][16:0]='hee_ee_ee_ee;
+  array[5]='haa_aa_aa_aa;
+  foreach(array[i])begin
+    $display("data(%0d)=%0h",i,array[i]);
+  end
+  end
+endmodule
