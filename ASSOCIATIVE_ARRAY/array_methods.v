a1864a43e33346d5a518b8556accf7698c2c8c8e
Megalan <megalan4444@gmail.com>
2026-03-23 16:17:11 +0530

    Update and rename num_method.v to array_methods.v

---
+++ ASSOCIATIVE_ARRAY/array_methods.v
@@ -0,0 +1,26 @@
+module test;
+  int marks[string];
+  initial begin
+    marks["tamil"]=90;
+    marks["science"]=77;
+    marks["math"]=66;
+    $display("Total subjects=%0d",i,marks.num());
+  end
+endmodule
+
+//remaining methods
+module test;
+  int marks[string];
+  initial begin
+    marks["tamil"]=90;
+    marks["science"]=77;
+    marks["social science"]=88;
+    marks["math"]=66;
+    if(marks.exists("math"))
+      $display("math exists");
+    marks.delete("science");
+    foreach(marks[i])begin
+      $display("marks(%s)=%0d",i,marks[i]);
+    end
+  end
+endmodule
--- ASSOCIATIVE_ARRAY/num_method.v
+++
@@ -1,9 +0,0 @@
-module test;
-  int marks[string];
-  initial begin
-    marks["tamil"]=90;
-    marks["science"]=77;
-    marks["math"]=66;
-    $display("Total subjects=%0d",i,marks.num());
-  end
-endmodule
