928d66c772ec787d829ccae243e4685f4978b06b
Megalan <megalan4444@gmail.com>
2026-04-06 20:30:49 +0530

    Add output comments for one_d array values

--- unpacked_array/1d_unpacked_array.sv
+++ unpacked_array/1d_unpacked_array.sv
@@ -11,3 +11,12 @@ module tb;
     end
   end
 endmodule
+
+//output
+one_d arry=0          0
+one_d arry=1          2
+one_d arry=2          4
+one_d arry=3          6
+one_d arry=4          8
+one_d arry=5         10
+one_d arry=6         12
