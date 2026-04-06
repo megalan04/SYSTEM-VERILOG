51ca1e65e54df5679d858f414a7458dd5b3bbae5
Megalan <megalan4444@gmail.com>
2026-04-06 20:32:39 +0530

    Add output initialization for 3D unpacked array

--- unpacked_array/3d_unpacked_array.sv
+++ unpacked_array/3d_unpacked_array.sv
@@ -20,3 +20,65 @@ module tb;
     end
   end
 endmodule
+
+//output
+three_d arry[0][0][0]=0
+three_d arry[0][0][1]=1
+three_d arry[0][0][2]=2
+three_d arry[0][0][3]=3
+three_d arry[0][1][0]=1
+three_d arry[0][1][1]=2
+three_d arry[0][1][2]=3
+three_d arry[0][1][3]=4
+three_d arry[0][2][0]=2
+three_d arry[0][2][1]=3
+three_d arry[0][2][2]=4
+three_d arry[0][2][3]=5
+three_d arry[1][0][0]=2
+three_d arry[1][0][1]=3
+three_d arry[1][0][2]=4
+three_d arry[1][0][3]=5
+three_d arry[1][1][0]=3
+three_d arry[1][1][1]=4
+three_d arry[1][1][2]=5
+three_d arry[1][1][3]=6
+three_d arry[1][2][0]=4
+three_d arry[1][2][1]=5
+three_d arry[1][2][2]=6
+three_d arry[1][2][3]=7
+three_d arry[2][0][0]=4
+three_d arry[2][0][1]=5
+three_d arry[2][0][2]=6
+three_d arry[2][0][3]=7
+three_d arry[2][1][0]=5
+three_d arry[2][1][1]=6
+three_d arry[2][1][2]=7
+three_d arry[2][1][3]=8
+three_d arry[2][2][0]=6
+three_d arry[2][2][1]=7
+three_d arry[2][2][2]=8
+three_d arry[2][2][3]=9
+three_d arry[3][0][0]=6
+three_d arry[3][0][1]=7
+three_d arry[3][0][2]=8
+three_d arry[3][0][3]=9
+three_d arry[3][1][0]=7
+three_d arry[3][1][1]=8
+three_d arry[3][1][2]=9
+three_d arry[3][1][3]=10
+three_d arry[3][2][0]=8
+three_d arry[3][2][1]=9
+three_d arry[3][2][2]=10
+three_d arry[3][2][3]=11
+three_d arry[4][0][0]=8
+three_d arry[4][0][1]=9
+three_d arry[4][0][2]=10
+three_d arry[4][0][3]=11
+three_d arry[4][1][0]=9
+three_d arry[4][1][1]=10
+three_d arry[4][1][2]=11
+three_d arry[4][1][3]=12
+three_d arry[4][2][0]=10
+three_d arry[4][2][1]=11
+three_d arry[4][2][2]=12
+three_d arry[4][2][3]=13
