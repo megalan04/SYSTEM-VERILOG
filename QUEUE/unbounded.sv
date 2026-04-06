bd35bc79373741963329d3563c83d6ee646131c0
Megalan <megalan4444@gmail.com>
2026-04-06 20:29:52 +0530

    Update unbounded.sv

--- QUEUE/unbounded.sv
+++ QUEUE/unbounded.sv
@@ -4,24 +4,40 @@ module queue;
   initial begin
     j=1;
     q.insert(1,j);
-    $display(q);
+    $display("%p",q);
     q.delete(1);
-    $display(q);
+    $display("%p",q);
     q.push_front(7);
-    $display(q);
+    $display("%p",q);
     q.push_back(9);
-    $display(q);
+    $display("%p",q);
     j=q.pop_back();
-    $display(j,",",q);
+    $display("%p",q);
+    $display("%0d,%p",j,q);
     j=q.pop_front();
-    $display(j,",",q);
+    $display("%p",q);
+    $display("%0d,%p",j,q);
     q.reverse();
-    $display(q);
+    $display("%p",q);
     q.sort();
-    $display(q);
+    $display("%p",q);
     q.rsort();
-    $display(q);
+    $display("%p",q);
     q.shuffle();
-    $display(q);
+    $display("%p",q);
   end
 endmodule
+
+//output
+'{0, 1, 2, 3, 4}
+'{0, 2, 3, 4}
+'{7, 0, 2, 3, 4}
+'{7, 0, 2, 3, 4, 9}
+'{7, 0, 2, 3, 4}
+9,'{7, 0, 2, 3, 4}
+'{0, 2, 3, 4}
+7,'{0, 2, 3, 4}
+'{4, 3, 2, 0}
+'{0, 2, 3, 4}
+'{4, 3, 2, 0}
+'{4, 3, 2, 0}
