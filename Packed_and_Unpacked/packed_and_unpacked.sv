a7c565b648d5cbedc91cc46aeaaac5d930a55240
Megalan <megalan4444@gmail.com>
2026-04-06 20:11:35 +0530

    Update packed_and_unpacked.sv

--- Packed_and_Unpacked/packed_and_unpacked.sv
+++ Packed_and_Unpacked/packed_and_unpacked.sv
@@ -2,11 +2,24 @@ module unpacked_1d;
   logic[31:0] array[7:0];
   int i;
   initial begin
+    foreach(array[i])begin
+      array[i]=32'h0;
+    end
   array[4]='hdd_dd_dd_dd;
     array[7][16:0]='hee_ee_ee_ee;
   array[5]='haa_aa_aa_aa;
   foreach(array[i])begin
-    $display("data(%0d)=%0h",i,array[i]);
+    $display("data(%0d)=%h",i,array[i]);
   end
   end
 endmodule
+
+//output
+data(7)=0000eeee
+data(6)=00000000
+data(5)=aaaaaaaa
+data(4)=dddddddd
+data(3)=00000000
+data(2)=00000000
+data(1)=00000000
+data(0)=00000000
