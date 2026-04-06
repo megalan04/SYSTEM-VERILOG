3cb85a093198dbd39322306f19c1ee9ddbc000d8
Megalan <megalan4444@gmail.com>
2026-04-06 20:31:40 +0530

    Add output values for two_d array
    Added output values for two-dimensional array.

--- unpacked_array/2d_unpacked_array.sv
+++ unpacked_array/2d_unpacked_array.sv
@@ -16,3 +16,17 @@ module tb;
     end
   end
 endmodule
+
+//output
+two_d arry[0][0]=0
+two_d arry[0][1]=1
+two_d arry[0][2]=2
+two_d arry[0][3]=3
+two_d arry[1][0]=2
+two_d arry[1][1]=3
+two_d arry[1][2]=4
+two_d arry[1][3]=5
+two_d arry[2][0]=4
+two_d arry[2][1]=5
+two_d arry[2][2]=6
+two_d arry[2][3]=7
