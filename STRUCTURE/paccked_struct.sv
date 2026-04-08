bb43a8a3193b2a94ac4dca2cdb424e64d8ebe7b3
Megalan <megalan4444@gmail.com>
2026-04-08 19:26:47 +0530

    Create paccked_struct.sv

---
+++ STRUCTURE/paccked_struct.sv
@@ -0,0 +1,21 @@
+//packed_struct
+module packed_tb;
+  struct packed{
+    bit[7:0]a;
+    bit[3:0]b; 
+    bit c;
+  }value;
+  initial 
+    begin
+      value='{8'hfa,4'he,2};
+      $display("%p",value);
+      
+      value.b=4'h2;
+      $display("%p",value.b);
+      $display("%p",value);
+      
+      value.c=15;
+      $display("%p",value.c);
+      $display("%p",value);
+    end
+endmodule
