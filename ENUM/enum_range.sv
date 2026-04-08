01d3fd805ca51fa4a4bb590a54640c25096a2ac8
Megalan <megalan4444@gmail.com>
2026-04-08 17:07:19 +0530

    Add SystemVerilog testbench for enum types

---
+++ ENUM/enum_range.sv
@@ -0,0 +1,60 @@
+module enum_tb;
+  typedef enum{CAPTAIN_AMERICA,THOR,HULK,SPIDERMAN,IRONMAN,THANOS,GROOT,AVENGERS_ASSEMBLE}characters;
+  characters a;
+  int i;
+  initial begin
+    $display("----------------a-----------------");
+    for(i=0;i<8;i++)begin
+      a=characters'(i);
+      $display("value=%0d character=%s",a,a.name());
+    end
+  end
+
+ 
+  typedef enum{CAPTAIN_AMERICA1,THOR1=4,HULK1,SPIDERMAN1,IRONMAN1,THANOS1,GROOT1,AVENGERS_ASSEMBLE1}characters1;
+  characters1 b;
+  initial begin
+    $display("----------------b-----------------");
+    for(i=0;i<8;i++)begin
+      b=characters1'(i);
+      $display("value=%0d character=%s",b,b.name());
+    end
+  end
+  
+
+
+
+  typedef enum{CAPTAIN_AMERICA2,THOR2,HULK2,SPIDERMAN2[4],IRONMAN2,THANOS2,GROOT2,AVENGERS_ASSEMBLE2}characters2;
+  characters2 c;
+  initial begin
+     $display("----------------c-----------------");
+    for(i=0;i<8;i++)begin
+      c=characters2'(i);
+      $display("value=%0d character=%s",c,c.name());
+    end
+  end
+  
+
+
+  typedef enum{CAPTAIN_AMERICA3,THOR3,HULK3[3:5],SPIDERMAN3,IRONMAN3,THANOS3,GROOT3,AVENGERS_ASSEMBLE3}characters3;
+  characters3 d;
+  initial begin
+     $display("----------------d-----------------");
+    for(i=0;i<8;i++)begin
+      d=characters3'(i);
+      $display("value=%0d character=%s",d,d.name());
+    end
+  end
+  
+ 
+  typedef enum{CAPTAIN_AMERICA4,THOR4,HULK4,SPIDERMAN4,IRONMAN4=9,THANOS4,GROOT4,AVENGERS_ASSEMBLE4}characters4;
+  characters4 e;
+  initial begin
+     $display("----------------e-----------------");
+
+    for(i=0;i<8;i++)begin
+      e=characters4'(i);
+      $display("value=%0d character=%s",e,e.name());
+    end
+  end
+endmodule
