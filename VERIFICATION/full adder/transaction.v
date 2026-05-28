a9ca8b283c250649fde9130e8669f8022b4f1bae
Megalan <megalan4444@gmail.com>
2026-05-28 20:32:04 +0530

    Create transaction.v

---
+++ VERIFICATION/full adder/transaction.v
@@ -0,0 +1,13 @@
+class transaction;
+  rand bit A;
+  rand bit B;
+  rand bit Cin;
+  
+  bit Sum;
+  bit Cout;
+  
+  function void display(string MEK);
+    $display("[%s] A=%b B=%b Cin=%b Sum=%b Cout=%b",MEK,A,B,Cin,Sum,Cout);
+  endfunction
+endclass
+  
