fdcd2b82bb74ae877076878ff29c42ec9fb86b3e
Megalan <megalan4444@gmail.com>
2026-06-14 19:49:24 +0530

    Create fork join.sv

---
+++ FORK/fork join.sv
@@ -0,0 +1,21 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+initial begin
+  $display("Start Time=%0t",$time);
+  fork begin
+      #10;
+      $display("Process 1 completed at %0t",$time);
+  end
+    begin
+      #20;
+      $display("Process 2 completed at %0t",$time);
+    end
+    begin
+      #30;
+      $display("Process 3 completed at %0t",$time);
+    end
+  join
+  $display("All processes completed at %0t", $time);
+end
+endmodule
