6e743dd1be95f6477cc255927678ab18c55c3a5a
Megalan <megalan4444@gmail.com>
2026-06-14 19:48:42 +0530

    Create fork join any.sv

---
+++ FORK/fork join any.sv
@@ -0,0 +1,24 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+initial begin
+  $display("Start Time=%0t",$time);
+  fork begin
+      #10;
+      $display("Process 1 completed at %0t",$time);
+    end
+    begin
+      #20;
+      $display("Process 2 completed at %0t",$time);
+    end
+    begin
+      #30;
+      $display("Process 3 completed at %0t",$time);
+    end
+  join_any
+
+  $display("Parent continues at %0t",$time);
+
+end
+
+endmodule
