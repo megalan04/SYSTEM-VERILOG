fbfb06d0d3307752d8d100866165f5b2028172a2
Megalan <megalan4444@gmail.com>
2026-07-18 10:52:28 +0530

    Create Topmodule.sv

---
+++ VERIFICATION/ASYN_FIFIO/Design/Topmodule.sv
@@ -0,0 +1,53 @@
+// Code your design here
+`include "r_sync.sv" 
+`include "r_handler.sv"
+`include "w_handler.sv"
+`include "w_sync.sv"
+`include "fifo_mem.sv"
+
+module top #(parameter d_width = 8 , ptr_width = 4 )
+	(input wclk,rclk,wrst,rrst,we,re,
+	input [d_width-1:0] w_data,
+	output [d_width-1:0] r_dout,
+	output full ,empty);
+wire [ptr_width-1:0] g_wptr,g_rptr,b_wptr,b_rptr;
+wire [ptr_width-1:0] sync_gwptr,sync_grptr;
+
+w_rsync b1 (.rclk(rclk),
+	.rrst(rrst),
+	.g_wptr(g_wptr),
+	.sync_gwptr(sync_gwptr));
+
+r_wsync b2 (.wclk(wclk),
+	.wrst(wrst),
+	.g_rptr(g_rptr),
+	.sync_grptr(sync_grptr));
+
+wptr_handler b3 (.wclk(wclk),
+	.wrst(wrst),
+	.we(we),
+	.sync_grptr(sync_grptr),
+	.b_wptr(b_wptr),
+	.g_wptr(g_wptr),
+	.full(full));
+
+rptr_handler b4 (.rclk(rclk),
+	.rrst(rrst),
+	.re(re),
+	.sync_gwptr(sync_gwptr),
+	.b_rptr(b_rptr),
+	.g_rptr(g_rptr),
+	.empty(empty));
+
+fifo_mem b5 (.wclk(wclk),
+	.rclk(rclk),
+	.we(we),
+	.re(re),
+	.full(full),
+	.empty(empty),
+	.b_wptr(b_wptr),
+	.b_rptr(b_rptr),
+	.w_data(w_data),
+	.r_dout(r_dout));
+
+endmodule
