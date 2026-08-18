c3c7393ca163ae55c32b40bedd7b5b8a63a4b840
Megalan <megalan4444@gmail.com>
2026-08-18 08:57:40 +0530

    Add SystemVerilog packet class with random constraint

---
+++ CONSTRAINT/range constraint.sv
@@ -0,0 +1,29 @@
+class packet;
+  rand int data;
+  constraint c1{data inside{[10:20]};
+               }
+endclass
+
+module tb;
+  packet p;
+  initial begin
+    p=new();
+    repeat(10)begin
+      p.randomize();
+      $display("data=%0d",p.data);
+    end
+  end
+endmodule
+
+
+//output
+data=16
+data=12
+data=13
+data=11
+data=18
+data=10
+data=19
+data=18
+data=16
+data=14
