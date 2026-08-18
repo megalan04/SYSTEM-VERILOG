bf1d27ea88584665d9a7e6d7a9e8cd5c2f0daa73
Megalan <megalan4444@gmail.com>
2026-08-18 09:00:39 +0530

    Add SystemVerilog packet class with distribution constraint

---
+++ CONSTRAINT/dist constraint.sv
@@ -0,0 +1,22 @@
+class packet;
+  rand int data;
+  constraint c1{data dist{10:=30,20:=60,30:=10};
+  }
+endclass
+
+module tb;
+  packet p;
+  initial begin
+    p=new();
+    repeat(4)begin
+      p.randomize();
+      $display("data=%0d",p.data);
+    end
+  end
+endmodule
+
+//output
+data=20
+data=20
+data=10
+data=10
