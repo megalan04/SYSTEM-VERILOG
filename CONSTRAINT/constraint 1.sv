48af9ac63ad567bdf13a3efd2bc85c174e2ce1d7
Megalan <megalan4444@gmail.com>
2026-08-18 08:54:44 +0530

    Create constraint 1.sv

---
+++ CONSTRAINT/constraint 1.sv
@@ -0,0 +1,25 @@
+class packet;
+  rand bit[3:0]addr;
+  constraint addr_c{
+    addr inside{[4:10]};
+    
+  }
+endclass
+
+module tb;
+  packet p;
+  initial begin
+    p=new();
+    repeat(4)begin
+      p.randomize();
+      $display("addr=%0d",p.addr);
+    end
+  end
+endmodule
+
+
+//output
+addr=6
+addr=4
+addr=10
+addr=8
