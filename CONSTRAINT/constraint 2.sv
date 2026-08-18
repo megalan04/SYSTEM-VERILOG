6799e920e2522ff2fcd115a7f54309864f0a6b90
Megalan <megalan4444@gmail.com>
2026-08-18 08:55:57 +0530

    Add SystemVerilog packet class with constraints

---
+++ CONSTRAINT/constraint 2.sv
@@ -0,0 +1,24 @@
+class packet;
+  rand int data;
+  constraint c1{data>44;
+                data<55;
+    
+  }
+endclass
+
+module tb;
+  packet p;
+  initial begin
+    p=new();
+    repeat(4)begin
+      p.randomize();
+      $display("addr=%0d",p.data);
+    end
+  end
+endmodule
+
+//output
+addr=45
+addr=53
+addr=50
+addr=52
