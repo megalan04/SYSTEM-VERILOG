1c715ed2d9b5048f2ff27c86cae1cb277c42b564
Megalan <megalan4444@gmail.com>
2026-08-19 09:20:46 +0530

    Add I2C clock generator module

---
+++ VERIFICATION/APB TO I2C/Design/i2c_clk_gen.sv
@@ -0,0 +1,34 @@
+module i2c_clk_gen
+(
+    input clk, rst_n, ena,
+    input [15:0] clk_cnt,
+    output reg int_tick
+);
+reg [15:0] counter;
+
+
+    always @(posedge clk) begin
+        if(!rst_n) begin
+            int_tick <= 0;
+            counter <= 0;
+        end
+
+        else if (ena) begin
+
+            if (counter == clk_cnt) begin
+                int_tick <= 1;
+                counter <= 0;
+            end
+            else begin
+                int_tick <= 0;
+                counter <= counter+1;
+            end
+        end
+        
+        else begin
+            int_tick <= 0;
+            counter <= 0;
+        end
+ 
+    end
+endmodule
